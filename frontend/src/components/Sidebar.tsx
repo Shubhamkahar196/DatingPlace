b844c281c397848f969b7aad5d2c64fe1684a386
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-30 16:58:09 +0530

    Added NoMatchesFound

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+import { MessageCircle, X } from "lucide-react";
+import LoadingState from "./LoadingState";
+import NoMatchesFound from "./NoMatchesFound";
+import {Link} from 'react-router-dom'
+import 
